6a824c71cde67fa37c528b70094b8a181e9b5d48
Roman Haas <haas@cqse.eu>
2022-03-10 17:46:47 +0100

    Add .NET options for zeroMQ in extension config

--- options.js
+++ options.js
@@ -18,4 +18,23 @@ function saveChanges(inputElement) {
     chrome.storage.local.set(storageObject);
 }
 
+function showTechnologyOptions(){
+    var technology = document.getElementById("technology");
+    var sap = document.getElementById("sap");
+    var dotnet = document.getElementById("dotnet");
+    var sapInput = document.getElementById("sap-input");
+    var dotnetInput = document.getElementById("dotnet-input")
+    
+    if ( technology.value == 'dotnet')
+      {
+        dotnetInput.hidden = false;
+        sapInput.hidden = true ;
+      }     
+        else
+      {
+        dotnetInput.hidden = true;
+        sapInput.hidden = false ;
+     }
+}
+
 loadDataAndListen();
